e04639414d2b2edc65a62cadd762ef9b8eadaa04
Jhon Achata Limachi <jhonachata.dev@gmail.com>
2022-10-29 16:44:01 +0000

    ejercicios base

--- bases/app.js
+++ bases/app.js
@@ -1,5 +1,35 @@
 "use strict";
 (function () {
-    var a = 10;
-    console.log(a);
+    // Tipos
+    var batman = 'Bruce';
+    var superman = 'Clark';
+    var existe = false;
+    // Tuplas
+    var parejaHeroes = [batman, superman];
+    var villano = ['Lex Lutor', 5, true];
+    // Arreglos
+    var aliados = ['Mujer Maravilla', 'Acuaman', 'San', 'Flash'];
+    //Enumeraciones
+    var FuerzaHeroes;
+    (function (FuerzaHeroes) {
+        FuerzaHeroes[FuerzaHeroes["acuaman"] = 0] = "acuaman";
+        FuerzaHeroes[FuerzaHeroes["batman"] = 1] = "batman";
+        FuerzaHeroes[FuerzaHeroes["flash"] = 5] = "flash";
+        FuerzaHeroes[FuerzaHeroes["superman"] = 100] = "superman";
+    })(FuerzaHeroes || (FuerzaHeroes = {}));
+    var fuerzaFlash = FuerzaHeroes.flash;
+    var fuerzaSuperman = FuerzaHeroes.superman;
+    var fuerzaBatman = FuerzaHeroes.batman;
+    var fuerzaAcuaman = FuerzaHeroes.acuaman;
+    // Retorno de funciones
+    function activar_batiseñal() {
+        return 'activada';
+    }
+    function pedir_ayuda() {
+        console.log('Auxilio!!!');
+    }
+    // Aserciones de Tipo
+    var poder = '100';
+    var largoDelPoder = poder.length;
+    console.log(largoDelPoder);
 })();
