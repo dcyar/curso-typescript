d6d61d9e841f8dfca64a332edd76b7e7316a13ee
Jhon Achata Limachi <jhonachata.dev@gmail.com>
2022-10-30 02:35:22 +0000

    objects types

---
+++ bases/objetos/objects.js
@@ -0,0 +1,17 @@
+"use strict";
+(function () {
+    var flash = {
+        name: "Barry Allen",
+        age: 24,
+        powers: ["Super velocidad", "Viajar en el tiempo"],
+    };
+    flash = {
+        name: "Clark Kent",
+        age: 60,
+        powers: ["Super fuerza"],
+        // getNombre() {
+        //     return this.name;
+        // },
+    };
+    console.log(flash);
+})();
---
+++ bases/objetos/objects.ts
@@ -0,0 +1,18 @@
+(() => {
+    let flash: { name: string; age: number; powers: string[] } = {
+        name: "Barry Allen",
+        age: 24,
+        powers: ["Super velocidad", "Viajar en el tiempo"],
+    };
+
+    flash = {
+        name: "Clark Kent",
+        age: 60,
+        powers: ["Super fuerza"],
+        // getNombre() {
+        //     return this.name;
+        // },
+    };
+
+    console.log(flash);
+})();
